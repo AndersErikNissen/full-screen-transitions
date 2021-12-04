4a2965f077525fcb96226be11d981a4c7c3eb09e
Anders <1086088@ucn.dk>
2021-12-04 23:56:50 +0100

    Work on first transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 //JS by Anders Erik Nissen
 //UCN MMDA0920
 
+const 
+btn = document.querySelector("#button"),
+t1_container = document.querySelector("#transition1__container");
+
+btn.addEventListener("click", ()=> {
+    console.log("asf")
+    if (!t1_container.classList.contains("active")) {
+        t1_container.classList.add("active")
+        if (t1_container.classList.contains("inactive")) {
+            t1_container.classList.remove("inactive")
+        }
+    } else {
+        t1_container.classList.add("inactive")
+        t1_container.classList.remove("active")
+    }
+})
+
+
+
+
 
 console.log("        /\\       _______   ___    __ ")
 console.log("       /  \\     |   ____| |   \\  |  |")
@@ -8,7 +28,3 @@ console.log("      / /\\ \\    |  |____  |    \\ |  |")
 console.log("     / /__\\ \\   |   ____| |  |\\ \\|  |")
 console.log("    / /____\\ \\  |  |____  |  | \\    |")
 console.log("   /_/      \\_\\ |_______| |__|  \\___|")
-
-
-
- 
